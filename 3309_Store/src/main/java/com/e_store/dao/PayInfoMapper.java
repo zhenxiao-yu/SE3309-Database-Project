85231b2179fe7267bdbbf161f981da5c04ca6b2e
zhenxiao-yu <markyu0615@gmail.com>
2021-10-01 18:29:40 -0400

    project directory change

---
+++ 3309_Store/src/main/java/com/e_store/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.e_store.dao;
+
+import com.e_store.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
---
+++ 3309_Store/src/main/java/com/e_store/service/IShippingService.java
@@ -0,0 +1,15 @@
+package com.e_store.service;
+
+import com.github.pagehelper.PageInfo;
+import com.e_store.common.ServerResponse;
+import com.e_store.pojo.Shipping;
+
+public interface IShippingService {
+
+    ServerResponse add(Integer userId, Shipping shipping);
+    ServerResponse<String> del(Integer userId,Integer shippingId);
+    ServerResponse update(Integer userId, Shipping shipping);
+    ServerResponse<Shipping> select(Integer userId, Integer shippingId);
+    ServerResponse<PageInfo> list(Integer userId, int pageNum, int pageSize);
+
+}
