4fe86d7fffe069736ac91503cf6d0fb29220218f
zhenxiao-yu <markyu0615@gmail.com>
2021-11-28 12:20:26 -0500

    first react component

---
+++ assignmnet4/src/3309-store/client/src/components/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//login component
+class Login extends React.Component {
+    render() {
+        return <p>login</p>;
+    }
+}
+
+export default Login;
--- assignmnet4/src/3309-store/client/src/index.js
+++ assignmnet4/src/3309-store/client/src/index.js
@@ -1,16 +1,8 @@
+//entry point js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Login from './components/Login'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ReactDOM.render(<Login/>, document.getElementById('root'));
