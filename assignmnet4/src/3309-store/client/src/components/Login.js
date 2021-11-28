13be5d20b692ac182d95d187967156083921cd11
zhenxiao-yu <markyu0615@gmail.com>
2021-11-28 14:02:25 -0500

    login page template update

--- assignmnet4/src/3309-store/client/src/components/Login.js
+++ assignmnet4/src/3309-store/client/src/components/Login.js
@@ -1,10 +1,31 @@
-import React from 'react';
-
+import React from "react";
+import '../css/store.scss';
 //login component
 class Login extends React.Component {
-    render() {
-        return <p>login</p>;
-    }
+  render() {
+    return (
+      <form className="login-container">
+        {/* Username input field */}
+        <div>
+          <label>Username</label>
+          <div>
+            <input type="text" placeholder="Username" />
+          </div>
+        </div>
+        {/* Password input field */}
+        <div>
+          <label>Password</label>
+          <div>
+            <input type="text" placeholder="Password" />
+          </div>
+        </div>
+        {/* Submit Button */}
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default Login;
