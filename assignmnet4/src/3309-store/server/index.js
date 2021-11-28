f1fd61b1abfd8bb67dca9bcfd877ebc34f7ae5b9
zhenxiao-yu <markyu0615@gmail.com>
2021-11-28 11:58:56 -0500

    mysql + react + express setup

---
+++ assignmnet4/src/3309-store/server/index.js
@@ -0,0 +1,21 @@
+//get express package
+const express= require('express');
+//create express app instance
+const app = express();
+//using mysql
+const mysql = require('mysql');
+
+//setup db connection
+const db = mysql.createConnection({
+    //your db credentials
+    user: 'root',
+    host: 'localhost',
+    password: '', 
+    database: '' 
+});
+
+const port = 3001; //port number
+const server = `http://localhost:${port}`;
+app.listen(port, ()=> console.log(`Server started. Running at: ${server}`));
+
+//define apis below
