3527f9b394cfe96880ff74438f3ad5f88e79ab8d
zhenxiao-yu <markyu0615@gmail.com>
2021-11-27 22:52:24 -0500

    cleaned up default files that came with create react cli

--- assignmnet4/src/3309-store/backend/index.js
+++ assignmnet4/src/3309-store/backend/index.js
@@ -1,11 +1,18 @@
+//get express package
 const express = require('express');
+//create express app instance
 const app = express();
 const path = require('path');
 
+//connect server to client
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(3000);
+const port = 3000; //port number
+const server = `http://localhost:${port}`;
+app.listen(port, ()=> console.log(`Server started. Running at: ${server}`));
+
+//define APIs below
