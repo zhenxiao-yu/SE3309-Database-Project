8d08c47e304a970e14e44221c20ed4c0fc3a9670
zhenxiao-yu <markyu0615@gmail.com>
2021-11-28 17:28:23 -0500

    simplified header

--- assignmnet4/src/3309-store/client/src/components/Header.js
+++ assignmnet4/src/3309-store/client/src/components/Header.js
@@ -1,25 +1,42 @@
 import React from "react";
 
 // Header component
-class Header extends React.Component {
-  render() {
-    return (
-      <div className="header-container">
-        <div className="navigation">
-          {/* links at right side of the header */}
-          <div className="nav-left">
-            <a href="/">Home</a>
-            <a href="/">Wishlist</a>
-          </div>
-          {/* links at left side of the header */}
-          <div className="nav-right">
-            <a href="/">Login</a> 
-            <a href="/">Register</a>
-          </div>
+const Header = (props) => {
+  return (
+    <div className="header-container">
+      <div className="navigation">
+        {/* links at right side of the header */}
+        <div className="nav-left">
+          {/* redirect to home page */}
+          <a href="/">Home</a>
+          {/* redirect to user's Wishlist */}
+          <a href="/">Wishlist</a>
+          {/* redirect to user's Order*/}
+          <a href="/">Orders</a>
+          {/* redirect to user's Payment info */}
+          <a href="/">Payment</a>
+        </div>
+        {/* links at left side of the header */}
+        <div className="nav-right">
+          {/* render username when logged in */}
+          {/* render register/login button when logged out */}
+          {props.username ? (
+            <span className="username">
+              <i className="fa fa-user"></i>
+              {props.username}
+            </span>
+          ) : (
+            <React.Fragment>
+              {/* redirect to login page */}
+              <a href="/">Login</a>
+              {/* redirect to Register page */}
+              <a href="/">Register</a>
+            </React.Fragment>
+          )}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Header;
