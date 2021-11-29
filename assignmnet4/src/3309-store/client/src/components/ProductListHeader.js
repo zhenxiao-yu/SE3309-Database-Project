d3dd6a7426d4a8af3e47130d41442fa30b981556
zhenxiao-yu <markyu0615@gmail.com>
2021-11-28 20:20:07 -0500

    productlist header + search bar componet

--- assignmnet4/src/3309-store/client/src/components/ProductListHeader.js
+++ assignmnet4/src/3309-store/client/src/components/ProductListHeader.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React from "react";
 
 class ProductListHeader extends React.Component {
-    render() {
-        return (
-            <div className="products-header">
-                <p>Header</p>
+  render() {
+    return (
+      <div className="products-header">
+        {/* store title */}
+        <div className="store-name">3309 Store</div>
+        {/* search area */}
+        <div className="search-window">
+          <div className="field has-addons">
+            {/* search bar */}
+            <div className="control">
+              <input type="text" className="input search-bar" placeholder="Search"/>
             </div>
-        )
-    }
+            {/* clear button */}
+            <div className="control">
+              <button className="button">
+                <i className="fa fa-cancel"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* shopping cart */}
+        <div className="cart-div">
+            {/* icon */}
+            <i className="fa fa-shopping-cart"></i>
+            {/* current number of items in cart */}
+            <span className="cart-amount">(0)</span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ProductListHeader;
