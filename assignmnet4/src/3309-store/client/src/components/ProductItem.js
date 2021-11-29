44ec8c62cb5d369ad7f1ef538bcd1fb50ee847b1
zhenxiao-yu <markyu0615@gmail.com>
2021-11-29 15:02:57 -0500

    populate product using state

--- assignmnet4/src/3309-store/client/src/components/ProductItem.js
+++ assignmnet4/src/3309-store/client/src/components/ProductItem.js
@@ -2,20 +2,21 @@ import React from 'react'
 
 class ProductItem extends React.Component {
     render() {
+        const {prodName, price, stock, prodStatus, viewCount, category, image} = this.props.product;
         return (
             <div className="item-container">
                 {/* details/info about the product */}
                 <div className="item-content">
                     <div className="item-img-container">
                         <figure className="image is-4by3">
-                            <img src="https://cdn.vox-cdn.com/thumbor/6QTbhbmlSyMwl5E8y1aU4x3cTz8=/0x0:2040x1360/1200x800/filters:focal(857x517:1183x843)/cdn.vox-cdn.com/uploads/chorus_image/image/65003327/jbareham_1492_170228_0024.0.0.jpg" alt=""></img>
+                            <img src={image} alt={prodName}></img>
                         </figure>
                         <div className="item-tags">
-                            <p>13 in left stock</p>
-                            <p>546 views</p>
-                            <p>Electronics</p>
+                            <p>{stock}<span> left in stock</span></p>
+                            <p>{viewCount} <span> views</span></p>
+                            <p>{category}</p>
                         </div>
-                        <p className="item-name">Nintendo Switch</p>
+                        <p className="item-name">{prodName}</p>
                     </div>
                 </div>
                 {/* price + add to shopping cart */}
@@ -23,7 +24,7 @@ class ProductItem extends React.Component {
                     {/* price */}
                     <p className="price">
                         <span>&#36;</span>
-                        <span>400</span>
+                        <span>{price}</span>
                     </p>
                     {/* add to cart */}
                     <button className="item-btn">
--- assignmnet4/src/3309-store/client/src/components/ProductList.js
+++ assignmnet4/src/3309-store/client/src/components/ProductList.js
@@ -4,8 +4,24 @@ import ProductItem from "components/ProductItem";
 import CategoryList from "components/CategoryList";
 
 
+
+
+
 //ProductList component
 class ProductList extends React.Component {
+
+  //product item attributes
+
+  product = {
+    prodName: 'Nintendo Switch',
+    price:'400',
+    stock:'123',
+    prodStatus:'Normal',
+    viewCount:'316',
+    category:'Electronics',
+    image:'https://cdn.vox-cdn.com/thumbor/6QTbhbmlSyMwl5E8y1aU4x3cTz8=/0x0:2040x1360/1200x800/filters:focal(857x517:1183x843)/cdn.vox-cdn.com/uploads/chorus_image/image/65003327/jbareham_1492_170228_0024.0.0.jpg'
+  }
+
   render() {
     return (
       <div>
@@ -16,19 +32,20 @@ class ProductList extends React.Component {
           <div className="columns is-multiline is-desktop">
             {/* each column is 3 slots, thus 4 products per line */}
             <div className="column is-3">
-              <ProductItem/>
+              <ProductItem product={this.product}/>
+            </div>
+            {/* test */}
+            <div className="column is-3">
+              <ProductItem product={this.product}/>
             </div>
             <div className="column is-3">
-              <ProductItem/>
+              <ProductItem product={this.product}/>
             </div>
             <div className="column is-3">
-              <ProductItem/>
+              <ProductItem product={this.product}/>
             </div>
             <div className="column is-3">
-              <ProductItem/>
-            </div>
-            <div className="column is-3">
-              <ProductItem/>
+              <ProductItem product={this.product}/>
             </div>
           </div>
         </div>
