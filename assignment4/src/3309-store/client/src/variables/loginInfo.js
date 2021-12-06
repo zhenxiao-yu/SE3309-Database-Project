21b84bb3a41c1e143d0d0eb0aa2008ca86eb29d2
Andrew Blommestyn <71096624+ablomm@users.noreply.github.com>
2021-12-05 19:16:27 -0500

    loginInfo comments

--- assignment4/src/3309-store/client/src/variables/loginInfo.js
+++ assignment4/src/3309-store/client/src/variables/loginInfo.js
@@ -1,5 +1,7 @@
 
 //this object will hold the current username of who is logged in
 const loginInfo = {
-
+    //if a user is logged in then it will contain two properties:
+    //username - the username of the user logged in
+    //id - the id of the user logged in
 }
