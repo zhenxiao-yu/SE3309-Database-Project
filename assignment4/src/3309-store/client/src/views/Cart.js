a3d2244bab522f4e0f27951db438d66806d74a29
ivyd <iivy.dding@gmail.com>
2021-12-05 20:55:22 -0500

    create cart and orders view

---
+++ assignment4/src/3309-store/client/src/views/Cart.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "components/Header";
+
+
+//Cart component
+class Cart extends React.Component {
+  render() {
+    return (
+      <div className="App-container">
+        <Header username="" />
+       <h1>Cart</h1>
+      </div>
+    );
+  }
+}
+
+export default Cart;
---
+++ assignment4/src/3309-store/client/src/views/Orders.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "components/Header";
+
+
+//orders  component
+class Orders extends React.Component {
+  render() {
+    return (
+      <div className="App-container">
+        <Header username="" />
+       <h1>ORDERS</h1>
+      </div>
+    );
+  }
+}
+
+export default Orders;
