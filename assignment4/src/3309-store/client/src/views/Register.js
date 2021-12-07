7c76cfc2616c99f11104de3ea7c8cb06c7808d04
sabuazma <sabuazma@uwo.ca>
2021-12-06 21:48:43 -0500

    register__needfix

--- assignment4/src/3309-store/client/src/views/Register.js
+++ assignment4/src/3309-store/client/src/views/Register.js
@@ -17,7 +17,14 @@ class Register extends React.Component {
         userFlag: "1",
         failedRegistser: false
     };
-
+    handleChange = (event) => {
+        //monitor form input
+        const value = event.target.value;
+        const name = event.target.name;
+        this.setState({
+          [name]: value,
+        });
+    };
     //Register click event
     handleRegister = (event) => {
         //prevent default action
@@ -56,49 +63,56 @@ class Register extends React.Component {
             <div className="field">
               <label className="label">Username</label>
               <div className="control">
-                <input type="text" className="input" placeholder="Username" name="username"/>
+                <input type="text" className="input" placeholder="Username" name="username" value={this.state.username}
+                onChange={this.handleChange}/>
               </div>
             </div>
             {/* Password input field */}
             <div className="field">
               <label className="label">Password</label>
               <div className="control">
-                <input type="password" className="input" placeholder="Password" name="pass"/>
+                <input type="password" className="input" placeholder="Password" name="pass" value={this.state.pass}
+                onChange={this.handleChange}/>
               </div>
             </div>
             {/* Email input field */}
             <div className="field">
               <label className="label">Email</label>
               <div className="control">
-                <input type="email" className="input" placeholder="Email" name="email"/>
+                <input type="email" className="input" placeholder="Email" name="email" value={this.state.email}
+                onChange={this.handleChange}/>
               </div>
             </div>
             {/* Phone number input field */}
             <div className="field">
               <label className="label">Phone number</label>
               <div className="control">
-                <input type="phoneNum" className="input" placeholder="Phone number" name="phoneNum"/>
+                <input type="phoneNum" className="input" placeholder="Phone number" name="phoneNum" value={this.state.phoneNum}
+                onChange={this.handleChange}/>
               </div>
             </div>
             {/* Secure question input field */}
             <div className="field">
               <label className="label">Secure Question</label>
               <div className="control">
-                <input type="secureQ" className="input" placeholder="Secure Question" name="secureQ"/>
+                <input type="secureQ" className="input" placeholder="Secure Question" name="secureQ" value={this.state.secureQ}
+                onChange={this.handleChange}/>
               </div>
             </div>
             {/* Secure answer input field */}
             <div className="field">
               <label className="label">Secure Answer</label>
               <div className="control">
-                <input type="secureA" className="input" placeholder="Secure Answer" name="secureA"/>
+                <input type="secureA" className="input" placeholder="Secure Answer" name="secureA" value={this.state.secureA}
+                onChange={this.handleChange}/>
               </div>
             </div>
             {/* Store name input field */}
             <div className="field">
               <label className="label">Store Name</label>
               <div className="control">
-                <input type="storeName" className="input" placeholder="Store Name" name="storeName"/>
+                <input type="storeName" className="input" placeholder="Store Name" name="storeName" value={this.state.storeName}
+                onChange={this.handleChange}/>
               </div>
             </div>
             {/* Submit Button */}
